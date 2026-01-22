a18ef3084c2449db68f8e072600892395fb06a77
Cristina Borini <cristina.borini01@gmail.com>
2026-01-22 21:25:41 +0100

    Clean up init file to avoid issues with path
    Removed unnecessary path setup from init file.

--- MLOps/tests/__init__.py
+++ MLOps/tests/__init__.py
@@ -1,12 +1,1 @@
 # MLOps/tests/__init__.py
-import os
-import sys
-
-_TEST_ROOT = os.path.dirname(__file__)                    # MLOps/tests
-_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)               # MLOps
-_SRC_ROOT = os.path.join(_PROJECT_ROOT, "src")            # MLOps/src
-_MLOPS_ROOT = os.path.join(_SRC_ROOT, "mlops")             # MLOps/src/mlops
-
-sys.path.insert(0, _SRC_ROOT)
-
-_PATH_DATA = os.path.join(_MLOPS_ROOT)
